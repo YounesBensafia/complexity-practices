73a6da8b7cc455ec58cb713c0f52a35bed84329e
Younes BENSAFIA <114447089+YounesBensafia@users.noreply.github.com>
2024-10-12 01:29:18 +0100

    First complexity exercise (TP1)

---
+++ TP1/tp1.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+#include<stdbool.h>
+
+
+// N 
+
+bool premierN(long int N) {
+    
+    for (long int i = 2; i < N; i++) { 
+        if (N % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+
+}
+
+// N/2
+
+bool premierNsur2(long int N) {
+    
+    for (long int i = 2; i < (N/2); i++) { 
+        if (N % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+
+}
+
+// sqrt(N)
+
+bool premierSqrtN(long int N) {
+    long int sq = sqrt(N);
+    for (long int i = 2; i <sq ; i++) { 
+        if (N % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+    
+}
+
+
+// Time execution Algo
+
+double testAlgo(bool (*callback)(long int), long int N)
+{
+    double debut = clock();
+
+    callback(N);
+
+    double fin = clock();
+    return (fin - debut) / CLOCKS_PER_SEC;
+}
+
+
+
+
+
+int main() {
+    FILE *fpt;
+    long int N[5] = {900004201, 99999, 903251, 22091, 1002583};
+
+    fpt = fopen("MyFile.csv", "w+");
+    fprintf(fpt, "'valeur', 'N', 'N/2', 'racine de N'\n");
+    fclose(fpt);
+
+    fpt = fopen("MyFile.csv", "a+");
+
+    for (long int i = 22091; i < 90202000; i=i+200)
+    {
+        double temps1 = testAlgo(premierN, i); 
+        double temps2 = testAlgo(premierNsur2, i); 
+        double temps3 = testAlgo(premierSqrtN, i); 
+
+        fprintf(fpt, "%ld, %f, %f, %f", i, temps1, temps2, temps3);
+        fprintf(fpt, "\n");
+    }
+
+    fclose(fpt);
+    
+
+    return 0;
+}
